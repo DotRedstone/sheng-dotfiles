20da0cfe9e048469f534aad7da861ab6d7e2d88e
DotRedstone <2724236945@qq.com>
2026-06-18 17:03:19 +0800

    fix(gnome): 移除抢占软键盘手势的 TouchUp

--- home/gnome/extensions.nix
+++ home/gnome/extensions.nix
@@ -11,7 +11,6 @@ let
     caffeine
     quick-settings-tweaker
     tiling-assistant
-    touchup
   ];
 in
 {
@@ -21,25 +20,6 @@ in
   };
 
   dconf.settings = {
-    "org/gnome/shell/extensions/touchup" = {
-      navigation-bar-enabled = true;
-      navigation-bar-mode = "gestures";
-      navigation-bar-gestures-reserve-space = true;
-      navigation-bar-gestures-invisible-mode = "never";
-      desktop-background-gestures-enabled = true;
-      overview-background-gestures-enabled = true;
-      window-preview-gestures-enabled = true;
-      osk-key-popups-enabled = true;
-      osk-key-popups-style = "accent";
-      osk-gestures-swipe-to-close-enabled = true;
-      osk-gestures-extend-keys-enabled = true;
-      osk-quick-paste-action-enabled = true;
-      osk-space-bar-ime-switching-enabled = true;
-      screen-rotate-utils-floating-screen-rotate-button-enabled = true;
-      double-tap-to-sleep-enabled = true;
-      notification-gestures-enabled = true;
-    };
-
     "org/gnome/shell/extensions/quick-settings-tweaks" = {
       notifications-enabled = true;
       notifications-compact = false;
